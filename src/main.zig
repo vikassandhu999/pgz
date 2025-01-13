9e2a0f9b0a0720a5090ba32c8b755d6ae8b9ff15
vikas sandhu <vikassandhu999@gmail.com>
2025-01-13 17:54:14 +0530

    add gitignore

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const time = @import("time");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
@@ -9,11 +10,11 @@ pub fn main() !void {
     // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
+    defer bw.flush();
+
     const stdout = bw.writer();
 
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {
@@ -21,4 +22,5 @@ test "simple test" {
     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
+    std.log.print("Test succesfull");
 }
